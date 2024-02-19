39e1fcd3b82cf7d77111876b90181e666e195a49
joseligo <129617242+joseligo@users.noreply.github.com>
2024-02-19 16:21:22 +0100

    Secure configDb.php

--- configDb.php
+++ configDb.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-  'db_name' => 'joseligo_ecf_garage_parrot',
-  'db_user' => 'joseligo',
-  'db_password' => 'Cameli@7685',
+  'db_name' => '',
+  'db_user' => 'root',
+  'db_password' => '',
   'db_port' => '3306',
-  'db_host' => 'mysql-joseligo.alwaysdata.net'
+  'db_host' => 'localhost'
 ];
